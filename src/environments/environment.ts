13e80557c8222df4828ecdc0a48cfbc76f303419
Rafael Carvalho <34134440+brollyc21@users.noreply.github.com>
2021-02-25 11:56:08 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAFZQct5C5kHGO4DR0gCriPUvo5p5DYHeA",
-    authDomain: "fb-clone-33ef0.firebaseapp.com",
-    databaseURL: "https://fb-clone-33ef0.firebaseio.com",
-    projectId: "fb-clone-33ef0",
-    storageBucket: "fb-clone-33ef0.appspot.com",
-    messagingSenderId: "158890497409",
-    appId: "1:158890497409:web:28854533fcb210108a4480"
+    apiKey: "a",
+    authDomain: "a",
+    databaseURL: "a",
+    projectId: "a",
+    storageBucket: "a",
+    messagingSenderId: "a",
+    appId: "a"
   }
 };
 
