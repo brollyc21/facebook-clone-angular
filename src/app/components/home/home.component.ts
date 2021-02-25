0a6c32565296a3742763f28c028803fb73cf51ca
Rafael Carvalho <34134440+brollyc21@users.noreply.github.com>
2021-02-25 11:58:21 +0000

    Add files via upload

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
+import { PostService } from '../../services/post.service';
+import { Subscription } from 'rxjs';
+import { AuthService, UserData } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit, OnDestroy {
   images: any[] = [
     'https://images-na.ssl-images-amazon.com/images/I/51DR2KzeGBL._AC_.jpg',
     'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg',
@@ -17,14 +18,44 @@ export class HomeComponent implements OnInit {
     'https://c0.wallpaperflare.com/preview/956/761/225/5be97da101a3f.jpg',
     'https://upload.wikimedia.org/wikipedia/commons/9/9a/Swepac_FB_465%2C_RV70%2C_with_passing_lorry.jpg'
   ];
+  subs: Subscription[] = [];
+  posts: any[] = [];
+  user: UserData;
 
-  constructor() { }
+  constructor(private postService: PostService,
+              private authService: AuthService) {
+  }
 
   ngOnInit(): void {
+    this.subs.push(this.postService.getAllPosts().subscribe(posts => {
+      this.posts = posts;
+    }));
+
+    this.subs.push(this.authService.CurrentUser().subscribe(user => {
+      this.user = user;
+    }));
+
+  }
+
+  ngOnDestroy(): void {
+    this.subs.map(s => s.unsubscribe());
+
   }
 
   postMessage(form: NgForm): void {
-
+    const {message} = form.value;
+    this.postService.postMessage(message,
+      `${this.user.firstName} ${this.user.lastName}`,
+      {
+        avatar: this.user.avatar,
+        lastName: this.user.lastName,
+        firstname: this.user.firstName
+      },
+    );
+    form.resetForm();
   }
 
+  logout(): void {
+    this.authService.Logout();
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<RegisterComponent>) { }
 
   ngOnInit(): void {
   }
